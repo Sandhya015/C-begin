f97644b2ddc3a1399304dc8b2b3b28634827ec0a
Sandhya <144054124+sandhya-1234@users.noreply.github.com>
2024-02-02 00:01:36 +0530

    Add files via upload

---
+++ C-begin/area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int l,b,res;
+l=7;
+b=5;
+int perimeter=0.5*l*b;
+int area=l*b;
+printf("perimeter of rectangle is=%d\n",perimeter);
+printf("area of rectangle is\n=%d",area);
+}
