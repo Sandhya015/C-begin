71ed08c21254c95d8300605c7a5d02306d14f1c1
Sandhya <144054124+sandhya-1234@users.noreply.github.com>
2024-02-02 00:02:05 +0530

    Add files via upload

---
+++ C-begin/dob.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+char name;
+int  dob, mobilenumber;
+printf("enter your name,dob,mobilenumber");
+scanf("%d %d %d",&name,&dob,&mobilenumber);
+}
---
+++ C-begin/reversal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+char char1='X';
+char char2='M';
+char char3='L';
+
+printf("the reversal of %c%c%c is %c%c%c\n",
+char1,char2,char3,
+char3,char2,char1);
+
+}
