035504b6e8cdf272a37af9b2ef11f4941991992a
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-02-29 07:23:18 +0530

    Add files via upload

---
+++ C-begin/triplesum.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int test(int x, int y); 
+int main(void)
+{ 
+  printf("%d", test(2, 2));
+  printf("\n");
+  printf("%d", test(2, 2));
+}
+
+int test(int x, int y)
+{
+
+   return x == y ? (x + y) * 3 : x + y;
+}
+
